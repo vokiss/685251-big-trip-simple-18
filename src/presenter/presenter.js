f7f7f74dae7fc7544d11e0cbefb2cd95eec7b2c6
vokiss <vokisskotov@gmail.com>
2022-08-30 15:35:52 +0300

    Революция или эволюция? (часть 1)

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -1,8 +1,8 @@
 import SortView from '../view/sort-view.js';
-import addPointView from '../view/add-point-view.js';
 import tripEventView from '../view/trip-event-view.js';
 import {render} from '../render.js';
 import EmptyListView from '../view/empty-list-view.js';
+import EditPointView from '../view/edit-point-view.js';
 
 export default class BoardPresenter {
   #emptyListComponent = new EmptyListView();
@@ -17,8 +17,6 @@ export default class BoardPresenter {
 
     render(new SortView(), this.#boardContainer);
     render(this.#emptyListComponent, this.#boardContainer);
-    render(new addPointView(this.#eventList[0]), this.#emptyListComponent.element);
-
     for (let i = 0; i < this.#eventList.length; i++) {
       this.#renderEvent(this.#eventList[i]);
     }
@@ -27,7 +25,7 @@ export default class BoardPresenter {
 
   #renderEvent = (event) => {
     const eventComponent = new tripEventView(event);
-    const editPointView = new addPointView(event);
+    const editPointView = new EditPointView(event);
 
     const replaceEventToEdit = () => {
       this.#emptyListComponent.element.replaceChild(editPointView.element, eventComponent.element);
@@ -35,12 +33,29 @@ export default class BoardPresenter {
     const replaceEditToEvent = () => {
       this.#emptyListComponent.element.replaceChild(eventComponent.element, editPointView.element);
     };
-    eventComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
-      replaceEventToEdit();
-    });
-    editPointView.element.querySelector('.event__reset-btn').addEventListener('click', () => {
-      replaceEditToEvent();
-    });
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceEditToEvent();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+    eventComponent.element.querySelector('.event__rollup-btn')
+      .addEventListener('click', () => {
+        replaceEventToEdit();
+        document.addEventListener('keydown', onEscKeyDown);
+      });
+    editPointView.element.querySelector('.event__rollup-btn')
+      .addEventListener('click', () => {
+        replaceEditToEvent();
+        document.removeEventListener('keydown', onEscKeyDown);
+      });
+    editPointView.element.querySelector('form')
+      .addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        replaceEditToEvent();
+        document.removeEventListener('keydown', onEscKeyDown);
+      });
 
     render(eventComponent, this.#emptyListComponent.element);
   };
--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -1,13 +1,33 @@
 import {createElement} from '../render.js';
 
-const editPointElement = () => (
-  `<li class="trip-events__item">
+const generateOffersTemplate = (count ,type, price, title) => {
+  const template = `<div class="event__offer-selector">
+            <input class="event__offer-checkbox  visually-hidden" id="event-offer-${type}-1" type="checkbox" name="event-offer-luggage" unchecked>
+            <label class="event__offer-label" for="event-offer-${type}-1">
+              <span class="event__offer-title">${title}</span>
+              &plus;&euro;&nbsp;
+              <span class="event__offer-price">${price}</span>
+            </label>
+          </div>
+          `;
+  let offers = [];
+  for (let i = 0; i < count; i++) {
+    offers += template;
+  }
+  return offers;
+};
+
+const editPointElement = (event) => {
+  const {name, type, description} = event.destination;
+  const {title, price} = event.offerByType.offer;
+  return (
+    `<li class="trip-events__item">
   <form class="event event--edit" action="#" method="post">
     <header class="event__header">
       <div class="event__type-wrapper">
         <label class="event__type  event__type-btn" for="event-type-toggle-1">
           <span class="visually-hidden">Choose event type</span>
-          <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
+          <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
         </label>
         <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
 
@@ -65,9 +85,9 @@ const editPointElement = () => (
 
       <div class="event__field-group  event__field-group--destination">
         <label class="event__label  event__type-output" for="event-destination-1">
-          Flight
+        ${type}
         </label>
-        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Chamonix" list="destination-list-1">
+        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${name}" list="destination-list-1">
         <datalist id="destination-list-1">
           <option value="Amsterdam"></option>
           <option value="Geneva"></option>
@@ -102,66 +122,29 @@ const editPointElement = () => (
         <h3 class="event__section-title  event__section-title--offers">Offers</h3>
 
         <div class="event__available-offers">
-          <div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage" checked>
-            <label class="event__offer-label" for="event-offer-luggage-1">
-              <span class="event__offer-title">Add luggage</span>
-              &plus;&euro;&nbsp;
-              <span class="event__offer-price">50</span>
-            </label>
-          </div>
-
-          <div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-comfort-1" type="checkbox" name="event-offer-comfort" checked>
-            <label class="event__offer-label" for="event-offer-comfort-1">
-              <span class="event__offer-title">Switch to comfort</span>
-              &plus;&euro;&nbsp;
-              <span class="event__offer-price">80</span>
-            </label>
-          </div>
-
-          <div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-meal-1" type="checkbox" name="event-offer-meal">
-            <label class="event__offer-label" for="event-offer-meal-1">
-              <span class="event__offer-title">Add meal</span>
-              &plus;&euro;&nbsp;
-              <span class="event__offer-price">15</span>
-            </label>
-          </div>
-
-          <div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-seats-1" type="checkbox" name="event-offer-seats">
-            <label class="event__offer-label" for="event-offer-seats-1">
-              <span class="event__offer-title">Choose seats</span>
-              &plus;&euro;&nbsp;
-              <span class="event__offer-price">5</span>
-            </label>
-          </div>
-
-          <div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-train-1" type="checkbox" name="event-offer-train">
-            <label class="event__offer-label" for="event-offer-train-1">
-              <span class="event__offer-title">Travel by train</span>
-              &plus;&euro;&nbsp;
-              <span class="event__offer-price">40</span>
-            </label>
-          </div>
+        ${generateOffersTemplate(5, type, price, title)}
         </div>
       </section>
 
       <section class="event__section  event__section--destination">
         <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-        <p class="event__destination-description">Chamonix-Mont-Blanc (usually shortened to Chamonix) is a resort area near the junction of France, Switzerland and Italy. At the base of Mont Blanc, the highest summit in the Alps, it's renowned for its skiing.</p>
+        <p class="event__destination-description">${description}</p>
       </section>
     </section>
   </form>
 </li>`
-);
+  ); };
 
-export default class editPointView {
+export default class EditPointView {
   #element = null;
-  get template() {
-    return editPointElement();
+  #event = null;
+
+  constructor(event) {
+    this.#event = event;
+  }
+
+  get template () {
+    return editPointElement(this.#event);
   }
 
   get element() {
