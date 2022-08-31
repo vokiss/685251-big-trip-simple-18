151bc595f06898abe486e4f3b883c378b32aa5f2
vokiss <vokisskotov@gmail.com>
2022-08-31 17:59:19 +0300

    Революция или эволюция? (часть 2)

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -1,7 +1,7 @@
 import {generateEvent} from '../mock/point-info.js';
 
 export default class EventsModel {
-  #events = Array.from({length: 5}, generateEvent);
+  #events = Array.from({length: 0}, generateEvent);
   get events () {
     return this.#events;
   }
