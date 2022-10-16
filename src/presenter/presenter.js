1e7553937b9dc6af8bf761a57815c68276d8bf3e
vokiss <vokisskotov@gmail.com>
2022-10-16 19:21:24 +0300

    yay! trip-event-presenter

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -1,13 +1,14 @@
 import {render} from '../framework/render.js';
 import SortView from '../view/sort-view.js';
-import TripEventView from '../view/trip-event-view.js';
 import EmptyListView from '../view/empty-list-view.js';
-import EditPointView from '../view/edit-point-view.js';
 import NoEventsView from '../view/no-events-view.js';
+import TripEventPresenter from './trip-event-presenter.js';
 
 
 export default class BoardPresenter {
   #emptyListComponent = new EmptyListView();
+  #emptyEventsComponent = new NoEventsView();
+  #sortTripEventsComponent = new SortView();
   #boardContainer = null;
   #eventsModel = null;
   #eventList = [];
@@ -16,48 +17,35 @@ export default class BoardPresenter {
     this.#boardContainer = boardContainer;
     this.#eventsModel = eventsModel;
     this.#eventList = [...this.#eventsModel.events];
+    this.#renderBoard();
+  };
 
+  #renderBoard = () => {
     if (this.#eventList.length === 0) {
-      render(new NoEventsView(), this.#boardContainer);
+      this.#renderEmptyEvents();
     } else {
-      render(new SortView(), this.#boardContainer);
-      render(this.#emptyListComponent, this.#boardContainer);
+      this.#renderSortTripComponent();
+      this.#renderEmptyListComponent();
       for (let i = 0; i < this.#eventList.length; i++) {
         this.#renderEvent(this.#eventList[i]);
       }
     }
   };
 
+  #renderEmptyListComponent = () => {
+    render(this.#emptyListComponent, this.#boardContainer);
+  };
+
+  #renderSortTripComponent = () => {
+    render(this.#sortTripEventsComponent, this.#boardContainer);
+  };
+
+  #renderEmptyEvents = () => {
+    render(this.#emptyEventsComponent, this.#boardContainer);
+  };
+
   #renderEvent = (event) => {
-    const eventComponent = new TripEventView(event);
-    const editPointView = new EditPointView(event);
-
-    const replaceEventToEdit = () => {
-      this.#emptyListComponent.element.replaceChild(editPointView.element, eventComponent.element);
-    };
-    const replaceEditToEvent = () => {
-      this.#emptyListComponent.element.replaceChild(eventComponent.element, editPointView.element);
-    };
-    const onEscKeyDown = (evt) => {
-      if (evt.key === 'Escape' || evt.key === 'Esc') {
-        evt.preventDefault();
-        replaceEditToEvent();
-        document.removeEventListener('keydown', onEscKeyDown);
-      }
-    };
-    eventComponent.setClickHandler(() => {
-      replaceEventToEdit();
-      document.addEventListener('keydown', onEscKeyDown);
-    });
-    editPointView.setEditClickHandler(() => {
-      replaceEditToEvent();
-      document.removeEventListener('keydown', onEscKeyDown);
-    });
-    editPointView.setFormSubmitHandler(() => {
-      replaceEditToEvent();
-      document.removeEventListener('keydown', onEscKeyDown);
-    });
-
-    render(eventComponent, this.#emptyListComponent.element);
+    const tripEventPresenter = new TripEventPresenter(this.#emptyListComponent.element);
+    tripEventPresenter.init(event);
   };
 }
